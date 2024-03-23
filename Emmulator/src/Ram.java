8ebdc5dfef0b5d200dda0318b5aa28a1a235cc81
GabrielAboumrad <153242822+GabrielAboumrad@users.noreply.github.com>
2024-03-23 15:00:19 -0400

    Update and rename ProgramWithIO.java to Ram.java

---
+++ Emmulator/src/Ram.java
@@ -0,0 +1,26 @@
+
+/**
+ * Put a short phrase describing the program here.
+ *
+ * @author Put your name here
+ *
+ */
+public final class Ram {
+
+    /**
+     * No argument constructor--private to prevent instantiation.
+     */
+    private Ram() {
+    }
+
+    /**
+     * Main method.
+     *
+     * @param args
+     *            the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+
+}
